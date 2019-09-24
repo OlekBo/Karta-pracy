a4a5d232e77e109328cf102ac445781a04ab721a
OlekBo <55761031+OlekBo@users.noreply.github.com>
2019-09-24 21:33:33 +0200

    Python 24.09.2019

---
+++ python katra pracy.py
@@ -0,0 +1,40 @@
+#1
+#print("podaj 2 liczby")
+#a=int(input())
+#b=int(input())
+#print(a*a+b*b)
+
+#2
+#print("podaj 2 liczby")
+#a=int(input())
+#b=int(input())
+#print((a+b)*(a+b))
+
+#3
+#print("podaj 2 liczby")
+#a=int(input())
+#b=int(input())
+#print((a-b)*(a-b)*(a-b))
+
+#4
+#print("podaj 3 liczby")
+#a=int(input())
+#b=int(input())
+#c=int(input())
+#print(a*b*c)
+
+#5
+#a=int(input())
+#b=int(input())
+#print(float((2*(a+b)/5)))
+#print("Panie Profesorze dlaczego nie mogę do float'a dać 0.2")
+
+#6
+#print("Podaj cenę brutto")
+#a=int(input())
+#print(float(a/(123/100)))
+
+#7
+#a=int(input())
+#b=int(input())
+#print(float(a%b))
